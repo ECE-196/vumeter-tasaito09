2d0ffbdbfb5750a17390977da0f3b2ec73726c42
tasaito09 <saitaku3116@gmail.com>
2024-04-22 15:22:34 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#print("Hello World!")
+
 import board
 from digitalio import DigitalInOut, Direction
 from analogio import AnalogIn
@@ -13,7 +15,14 @@ led_pins = [
     board.IO21,
     board.IO26, # type: ignore
     board.IO47,
-    # do the rest...
+    board.IO33,
+    board.IO34,
+    board.IO48,
+    board.IO35,
+    board.IO36,
+    board.IO37,
+    board.IO38,
+    board.IO39
 ]
 
 leds = [DigitalInOut(pin) for pin in led_pins]
@@ -27,10 +36,28 @@ while True:
 
     print(volume)
 
-    leds[0].value = not leds[0].value
-    leds[1].value = not leds[0].value
+    initial_volume = []
+    for _ in range(5):
+        initial_volume.append(microphone.value)
 
-    sleep(1)
+    #leds[0].value = not leds[0].value
+    #leds[1].value = not leds[0].value
+
+    default_volume = sum(initial_volume) / len(initial_volume)
+    led_step = 2000
+
+    for index, led in enumerate(leds):
+        index_volume = default_volume + (led_step*index)
+
+        if volume >= index_volume:
+            leds[index].value = 1
+        else:
+            leds[index].value = 0
+
+    sleep(0.2)
+
+
+
 
     # instead of blinking,
     # how can you make the LEDs
